da095ae4d34bd2dc0ea85570de2d6b5f47949f48
KarenAGM <kagm970412@gmail.com>
2021-01-22 20:11:31 -0600

    EjercicioTres

---
+++ src/KatasCode/kataUno/testKata1.java
@@ -0,0 +1,16 @@
+package KatasCode.kataUno;
+
+public class testKata1 {
+
+    public static void main(String[] args) {
+
+        Kata1 ejemplo1 = new Kata1();
+
+        String one = ejemplo1.convertir(new int[] {9,7,5,2,6,1,2,3,2,9});
+
+        System.out.println(one);
+
+
+
+    }
+}
